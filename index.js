edf80c7aaf7bbb14e79d1cc96f5975bb506f6f1f
Raylan Sales <raylanwork@gmail.com>
2020-11-13 00:40:52 -0300

    The prefix was added

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
+const prefix = ']';
+
 bot.login(config.token); //turn it on
 bot.on('ready', () =>{
     console.log('Im ready!');
 })
 
 bot.on('message', msg => {
-    if(msg.content.toLowerCase() === "fala tu"){
+    if(msg.content.toLowerCase() === prefix + "fala tu"){
         msg.channel.send('...working...');
-    }
+    }   
+
 })
+
+// const args = message.content.toLowerCase().slice(prefix.length).split(" "); //run multiple commands in a line
