670e12f17ff1178f05e61764a1bdd8104dd0e25a
Raylan Sales <raylanwork@gmail.com>
2020-11-12 23:19:23 -0300

    The token was hidded

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const config = require('./config.json');
+
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
-const token = 'Nzc2NjEzMjY4Njc1ODg3MTE0.X63beA.RZduFv5F5mvqWEsVUtnzX44fadM';
-
-bot.login(token); //turn it on
+bot.login(config.token); //turn it on
 bot.on('ready', () =>{
     console.log('Im ready!');
 })
